ee8fd2606743273e77ce9aa2656ec6df3084a801
PskCluB <passakon_p@hotmail.com>
2020-07-08 18:26:40 +0700

    Add json payload

--- app/domain/check_tx.go
+++ app/domain/check_tx.go
@@ -1,29 +1,19 @@
 package domain
 
 import (
-	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"github.com/tendermint/tendermint/abci/example/code"
 	"github.com/tendermint/tendermint/abci/types"
 )
 
+type Payload struct {
+	Name interface{}
+}
+
 func checkTX(app *Application, req types.RequestCheckTx) types.ResponseCheckTx {
-	fmt.Println(fmt.Sprintf("Invalid nonce. Expected >= %v, got %v", app.txCount, string(req.Tx)))
-	fmt.Println(req.Type)
-	if app.serial {
-		if len(req.Tx) > 8 {
-			return types.ResponseCheckTx{
-				Code: code.CodeTypeEncodingError,
-				Log:  fmt.Sprintf("Max tx size is 8 bytes, got %d", len(req.Tx))}
-		}
-		tx8 := make([]byte, 8)
-		copy(tx8[len(tx8)-len(req.Tx):], req.Tx)
-		txValue := binary.BigEndian.Uint64(tx8)
-		if txValue < uint64(app.txCount) {
-			return types.ResponseCheckTx{
-				Code: code.CodeTypeBadNonce,
-				Log:  fmt.Sprintf("Invalid nonce. Expected >= %v, got %v", app.txCount, txValue)}
-		}
-	}
+	data := &Payload{}
+	json.Unmarshal(req.Tx, data)
+	fmt.Println(fmt.Sprintf("Invalid nonce. Expected >= %v", data.Name))
 	return types.ResponseCheckTx{Code: code.CodeTypeOK}
 }
